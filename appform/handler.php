34e8c9206cc274f709821340d84f88bc1189ddc3
unknown <orshee_ertl@yahoo.com>
2020-10-19 14:14:06 +0200

    Land-a-job június utáni fejlesztés ➕ Mentorprogram

--- appform/handler.php
+++ appform/handler.php
@@ -21,6 +21,6 @@ $validator->field('comments')->maxLength(6000);
 
 
 
-$pp->sendEmailTo('mosoly@yourelegantmessage.com'); // ← Your email here
+$pp->sendEmailTo('land-a-job@yourelegantmessage.com'); // ← Your email here
 
 echo $pp->process($_POST);
